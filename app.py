6c2f28c83bf087d8382b2a3eef3603f5661a0d40
edydon <edydon@yahoo.com>
2025-02-02 01:13:44 -0500

    another commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, headintheclouds - this is our demo on Elastic Beanstalk!'
+	return 'Hello Edy - this is our demo on Elastic Beanstalk!'
 
 if __name__  == '__main__':
 	app.run( host = '0.0.0.0', port = 3000 )
